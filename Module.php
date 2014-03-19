f2fa4c8b8c2d3f32d3962ec9c9edfd7f12b0f89e
Dustin Moorman <dustin.moorman@gmail.com>
2014-03-18 22:07:54 -0500

    Adding Module.php

---
+++ Module.php
@@ -0,0 +1,16 @@
+<?php
+namespace WdgDoctrine2;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
